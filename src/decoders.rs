737a9266d8129d56f92014472019ffe407369398
Tsuyoshi Matsudate <t.matsudate@gmail.com>
2019-05-26 10:25:08 +0900

    4 Changed, 2 Removed, 1 Added:
    * Changed: Left the network processing to the handler.
    * Added: The chunk data which the result of Invoke(connect) will need.
    * Changed: Vec.get() was able to get its slice safely.
    * Changed: Followed the basic header/the message header changes.
    * Changed: The extended timestamp won't be needed yet when the timestamp value is just maximum of 24 bits.
    * Removed: Its 0xc3 byte is the basic header; its meant the `continue`.
    * Removed: the `decode_chunk()` has been currently unneeded.

--- src/decoders.rs
+++ src/decoders.rs
@@ -2,194 +2,169 @@ use std::{
     collections::{
         HashMap
     },
-    io::{
-        Error as IOError,
-        ErrorKind,
-        Read,
-        Result as IOResult,
-    },
-    net::{
-        TcpStream
-    },
     time::{
         Duration
     }
 };
 use crate::{
-    errors::{
-        ChunkLengthError
-    },
-    messages::*
+    messages::*,
 };
 
-pub(self) trait RtmpDecoder: GetByteBuffer {
+pub(crate) trait RtmpDecoder: GetByteBuffer {
     fn decode_basic_header(&mut self) -> Option<BasicHeader>;
-    fn decode_message_header(&mut self, basic_header: BasicHeader) -> Option<MessageHeader>;
+    fn decode_message_header(&mut self, message_format: MessageFormat) -> Option<MessageHeader>;
     fn decode_extended_timestamp(&mut self, message_header: MessageHeader) -> Option<Duration>;
+    fn decode_chunk_size(&mut self) -> Option<ChunkData>;
+    fn decode_bytes_read(&mut self) -> Option<ChunkData>;
+    fn decode_ping(&mut self) -> Option<ChunkData>;
     fn decode_server_bandwidth(&mut self) -> Option<ChunkData>;
+    fn decode_client_bandwidth(&mut self) -> Option<ChunkData>;
     fn decode_amf_number(&mut self) -> Option<AmfData>;
     fn decode_amf_boolean(&mut self) -> Option<AmfData>;
     fn decode_amf_string(&mut self) -> Option<AmfData>;
     fn decode_amf_object(&mut self) -> Option<AmfData>;
     fn decode_amf_data(&mut self) -> Option<AmfData>;
+    fn decode_invoke_connect_result(&mut self) -> Option<ChunkData>;
     fn decode_invoke(&mut self) -> Option<ChunkData>;
     fn decode_unknown(&mut self) -> Option<ChunkData>;
-    fn decode_chunk_data(&mut self, message_header: &MessageHeader) -> Option<ChunkData>;
-} 
+    fn decode_chunk_data(&mut self, message_type: MessageType) -> Option<ChunkData>;
+}
 
 impl GetByteBuffer for ByteBuffer {
     fn get_u8(&mut self) -> Option<u8> {
         let offset = self.offset();
-
-        if offset == self.len() {
-            return None;
-        }
-
-        let byte = self.bytes()[offset];
+        let n = self.bytes().get(offset).map(|b| *b);
 
         self.offset_to(1);
-        Some(byte)
+        n
     }
 
     fn get_u16_be(&mut self) -> Option<u16> {
         let offset = self.offset();
+        let n = self.bytes().get(offset..(offset + 2)).map(
+            |bytes| {
+                let mut ret: [u8; 2] = [0; 2];
 
-        if offset + 1 >= self.len() {
-            return None;
-        }
+                for i in 0..bytes.len() {
+                    ret[i] = bytes[i];
+                }
 
-        let mut bytes: [u8; 2] = [0; 2];
-        let s = &self.bytes()[offset..(offset + 2)];
-
-        for i in 0..bytes.len() {
-            bytes[i] = s[i];
-        }
+                u16::from_be_bytes(ret)
+            }
+        );
 
         self.offset_to(2);
-        Some(u16::from_be_bytes(bytes))
+        n
     }
 
     fn get_u16_le(&mut self) -> Option<u16> {
         let offset = self.offset();
+        let n = self.bytes().get(offset..(offset + 2)).map(
+            |bytes| {
+                let mut ret: [u8; 2] = [0; 2];
 
-        if offset + 1 >= self.len() {
-            return None;
-        }
+                for i in 0..bytes.len() {
+                    ret[i] = bytes[i];
+                }
 
-        let mut bytes: [u8; 2] = [0; 2];
-        let s = &self.bytes()[offset..(offset + 2)];
-
-        for i in 0..bytes.len() {
-            bytes[i] = s[i];
-        }
+                u16::from_le_bytes(ret)
+            }
+        );
 
         self.offset_to(2);
-        Some(u16::from_le_bytes(bytes))
+        n
     }
 
     fn get_u24_be(&mut self) -> Option<u32> {
         let offset = self.offset();
+        let n = self.bytes().get(offset..(offset + 3)).map(
+            |bytes| {
+                let mut ret: [u8; 4] = [0; 4];
 
-        if offset + 2 >= self.len() {
-            return None;
-        }
+                for i in 0..bytes.len() {
+                    ret[i + 1] = bytes[i];
+                }
 
-        let mut bytes: [u8; 4] = [0; 4];
-        let s = &self.bytes()[offset..(offset + 3)];
-
-        for i in 1..bytes.len() {
-            bytes[i] = s[i - 1];
-        }
+                u32::from_be_bytes(ret)
+            }
+        );
 
         self.offset_to(3);
-        Some(u32::from_be_bytes(bytes))
+        n
     }
 
     fn get_u32_be(&mut self) -> Option<u32> {
         let offset = self.offset();
+        let n = self.bytes().get(offset..(offset + 4)).map(
+            |bytes| {
+                let mut ret: [u8; 4] = [0; 4];
 
-        if offset + 3 >= self.len() {
-            return None;
-        }
+                for i in 0..bytes.len() {
+                    ret[i] = bytes[i];
+                }
 
-        let mut bytes: [u8; 4] = [0; 4];
-        let s = &self.bytes()[offset..(offset + 4)];
-
-        for i in 0..bytes.len() {
-            bytes[i] = s[i];
-        }
+                u32::from_be_bytes(ret)
+            }
+        );
 
         self.offset_to(4);
-        Some(u32::from_be_bytes(bytes))
+        n
     }
 
     fn get_u32_le(&mut self) -> Option<u32> {
         let offset = self.offset();
+        let n = self.bytes().get(offset..(offset + 4)).map(
+            |bytes| {
+                let mut ret: [u8; 4] = [0; 4];
 
-        if offset + 3 >= self.len() {
-            return None;
-        }
+                for i in 0..bytes.len() {
+                    ret[i] = bytes[i];
+                }
 
-        let mut bytes: [u8; 4] = [0; 4];
-        let s = &self.bytes()[offset..(offset + 4)];
-
-        for i in 0..bytes.len() {
-            bytes[i] = s[i];
-        }
+                u32::from_le_bytes(ret)
+            }
+        );
 
         self.offset_to(4);
-        Some(u32::from_le_bytes(bytes))
+        n
     }
 
     fn get_f64(&mut self) -> Option<f64> {
         let offset = self.offset();
+        let n = self.bytes().get(offset..(offset + 8)).map(
+            |bytes| {
+                let mut ret: [u8; 8] = [0; 8];
 
-        if offset + 7 >= self.len() {
-            return None;
-        }
+                for i in 0..bytes.len() {
+                    ret[i] = bytes[i];
+                }
 
-        let mut bytes: [u8; 8] = [0; 8];
-        let s = &self.bytes()[offset..(offset + 8)];
-
-        for i in 0..bytes.len() {
-            bytes[i] = s[i];
-        }
+                f64::from_bits(u64::from_be_bytes(ret))
+            }
+        );
 
         self.offset_to(8);
-        Some(f64::from_bits(u64::from_be_bytes(bytes)))
+        n
     }
 
     fn get_sliced_bytes(&mut self, len: usize) -> Option<Vec<u8>> {
         let offset = self.offset();
-
-        if offset + len - 1 >= self.len() {
-            return None;
-        }
-
-        let bytes = self.bytes()[offset..(offset + len)].to_vec();
+        let bytes = self.bytes().get(offset..(offset + len)).map(|bytes| bytes.to_vec());
 
         self.offset_to(len);
-        Some(bytes)
+        bytes
     }
 
-    fn peek_byte(&mut self) -> Option<u8> {
+    fn peek_byte(&self) -> Option<u8> {
         let offset = self.offset();
 
-        if offset == self.len() {
-            return None;
-        }
-
-        Some(self.bytes()[offset])
+        self.bytes().get(offset).map(|byte| *byte)
     }
 
-    fn peek_bytes(&mut self, len: usize) -> Option<Vec<u8>> {
+    fn peek_bytes(&self, len: usize) -> Option<Vec<u8>> {
         let offset = self.offset();
 
-        if offset + len - 1 >= self.len() {
-            return None;
-        }
-
-        Some(self.bytes()[offset..(offset + len)].to_vec())
+        self.bytes().get(offset..(offset + len)).map(|bytes| bytes.to_vec())
     }
 }
 
@@ -197,39 +172,37 @@ impl RtmpDecoder for ByteBuffer {
     fn decode_basic_header(&mut self) -> Option<BasicHeader> {
         self.get_u8().map(
             |b| {
-                use crate::messages::BasicHeader::*;
-
                 let message_format: MessageFormat = ((b & BasicHeader::MESSAGE_HEADER_FORMAT) >> 6).into();
                 let basic_header_type = b & BasicHeader::BASIC_HEADER_TYPE;
 
                 match basic_header_type {
-                    0 => TwoBytes {
+                    0 => BasicHeader::new(
                         message_format,
-                        channel_id: self.get_u8().unwrap() + 64
-                    },
-                    1 => ThreeBytes {
+                        ChunkId::U8(self.get_u8().unwrap() + 64)
+                    ),
+                    1 => BasicHeader::new(
                         message_format,
-                        channel_id: self.get_u16_le().unwrap() + 64
-                    },
-                    _ => OneByte {
+                        ChunkId::U16(self.get_u16_le().unwrap() + 64)
+                    ),
+                    _ => BasicHeader::new(
                         message_format,
-                        channel_id: basic_header_type
-                    }
+                        ChunkId::U8(basic_header_type)
+                    )
                 }
             }
         )
     }
 
-    fn decode_message_header(&mut self, basic_header: &BasicHeader) -> Option<MessageHeader> {
-        match basic_header.get_message_format() {
+    fn decode_message_header(&mut self, message_format: MessageFormat) -> Option<MessageHeader> {
+        match message_format {
             MessageFormat::New => if self.offset() + MessageHeader::LEN_NEW - 1 >= self.len() {
                 None
             } else {
                 Some(MessageHeader::New {
                     timestamp: Duration::from_secs(self.get_u24_be().unwrap() as u64),
-                    message_len: self.get_u24_be().unwrap() as usize,
+                    message_len: self.get_u24_be().unwrap(),
                     message_type: self.get_u8().unwrap().into(),
-                    channel_id: self.get_u32_le().unwrap()
+                    message_id: self.get_u32_le().unwrap()
                 })
             },
             MessageFormat::SameSource => if self.offset() + MessageHeader::LEN_SAME_SOURCE - 1 >= self.len() {
@@ -237,7 +210,7 @@ impl RtmpDecoder for ByteBuffer {
             } else {
                 Some(MessageHeader::SameSource {
                     timestamp: Duration::from_secs(self.get_u24_be().unwrap() as u64),
-                    message_len: self.get_u24_be().unwrap() as usize,
+                    message_len: self.get_u24_be().unwrap(),
                     message_type: self.get_u8().unwrap().into()
                 })
             },
@@ -252,19 +225,59 @@ impl RtmpDecoder for ByteBuffer {
         }
     }
 
-    fn decode_extended_timestamp(&mut self, message_header: &MessageHeader) -> Option<Duration> {
+    fn decode_extended_timestamp(&mut self, message_header: MessageHeader) -> Option<Duration> {
         message_header.get_timestamp().and_then(
-            |timestamp| if timestamp.as_secs() >= (U24_MAX as u64) {
-                Some(Duration::from_secs(self.get_u32_be().unwrap() as u64))
+            |timestamp| if timestamp.as_secs() > U24_MAX as u64 {
+                self.get_u32_be().map(
+                    |extended_timestamp| Duration::from_secs(extended_timestamp as u64)
+                )
             } else {
                 None
             }
         )
     }
 
+    fn decode_chunk_size(&mut self) -> Option<ChunkData> {
+        self.get_u32_be().map(
+            |chunk_size| {
+                if chunk_size & 0x80000000 != 0 {
+                    warn!("The most significant bit is 1!");
+                }
+
+                ChunkData::ChunkSize(chunk_size)
+            }
+        )
+    }
+
+    fn decode_bytes_read(&mut self) -> Option<ChunkData> {
+        self.get_u32_be().map(|bytes_read| ChunkData::BytesRead(bytes_read))
+    }
+
+    fn decode_ping(&mut self) -> Option<ChunkData> {
+        self.get_u16_be().and_then(
+            |ping_type_id| {
+                use crate::messages::PingType::*;
+
+                let ping_type: PingType = (ping_type_id as u8).into();
+
+                match ping_type {
+                    StreamBegin => self.get_u32_be().map(|stream_id| ChunkData::Ping(PingData::StreamBegin(stream_id))),
+                }
+            }
+        )
+    }
+
     fn decode_server_bandwidth(&mut self) -> Option<ChunkData> {
         self.get_u32_be().map(
-            |size| ChnukData::ServerBandwidth(size as usize)
+            |bandwidth| ChunkData::ServerBandwidth(bandwidth)
+        )
+    }
+
+    fn decode_client_bandwidth(&mut self) -> Option<ChunkData> {
+        self.get_u32_be().and_then(
+            |bandwidth| self.get_u8().map(
+                |limit_type| ChunkData::ClientBandwidth(bandwidth, limit_type.into())
+            )
         )
     }
 
@@ -304,14 +317,6 @@ impl RtmpDecoder for ByteBuffer {
             let value = self.decode_amf_data().unwrap();
 
             object.insert(key, value);
-
-            let byte = self.peek_byte().unwrap();
-
-            // a mysterious 'ﾃ'. Somehow this will appear before the object end sequence(== [0, 0, 9]).
-            // Currently this should skip because can't decode.
-            if byte == (0xc3 as u8) {
-                self.offset_to(1);
-            }
         }
 
         Some(AmfData::Object(object))
@@ -336,9 +341,42 @@ impl RtmpDecoder for ByteBuffer {
         )
     }
 
+    fn decode_invoke_connect_result(&mut self) -> Option<ChunkData> {
+        self.decode_amf_data().and_then(
+            |s| s.string().and_then(
+                |command| self.decode_amf_data().and_then(
+                    |n| n.number().map(
+                        |transaction_id| {
+                            let result: NetConnectionResult = command.into();
+                            let properties = self.decode_amf_data().and_then(
+                                |data| data.object().map(
+                                    |properties| properties
+                                )
+                            ).unwrap_or(HashMap::new());
+                            let information = self.decode_amf_data().and_then(
+                                |data| data.object().map(
+                                    |information| information
+                                )
+                            ).unwrap_or(HashMap::new());
+
+                            ChunkData::Invoke(
+                                InvokeCommand::NetConnection(
+                                    NetConnectionCommand::ConnectResult {
+                                        result,
+                                        transaction_id: transaction_id as u64,
+                                        properties,
+                                        information
+                                    }
+                                )
+                            )
+                        }
+                    )
+                )
+            )
+        )
+    }
+
     fn decode_invoke(&mut self) -> Option<ChunkData> {
-        let offset = self.offset();
-        println!("{:x?}", &self.bytes()[offset..]);
         self.decode_amf_data().and_then(
             |s| s.string().and_then(
                 |command| self.decode_amf_data().and_then(
@@ -361,15 +399,22 @@ impl RtmpDecoder for ByteBuffer {
                                     )
                                 ).unwrap();
 
-                                Some(Invoke(NetConnection(Connect {
-                                    argument: None,
-                                    transaction_id: transaction_id as u64,
-                                    command_object
-                                })))
+                                Some(
+                                    Invoke(
+                                        NetConnection(
+                                            Connect {
+                                                argument: None,
+                                                transaction_id: transaction_id as u64,
+                                                command_object
+                                            }
+                                        )
+                                    )
+                                )
                             } else {
                                 info!("Unknown invoke command: {}", command);
 
                                 let len = self.len();
+                                let offset = self.offset();
 
                                 self.get_sliced_bytes(len - offset).map(
                                     |bytes| Invoke(Unknown(bytes))
@@ -387,52 +432,21 @@ impl RtmpDecoder for ByteBuffer {
 
         let len = self.len();
         let offset = self.offset();
+
         Some(Unknown(self.get_sliced_bytes(len - offset).unwrap()))
     }
 
-    fn decode_chunk_data(&mut self, message_header: &MessageHeader) -> Option<ChunkData> {
-        message_header.get_message_type().and_then(
-            |message_type| {
-                use crate::messages::MessageType::*;
+    fn decode_chunk_data(&mut self, message_type: MessageType) -> Option<ChunkData> {
+        use crate::messages::MessageType::*;
 
-                match message_type {
-                    Invoke => self.decode_invoke(),
-                    _ => self.decode_unknown()
-                }
-            }
-        )
-    }
-}
-
-pub(crate) fn decode_chunk(stream: &mut TcpStream) -> IOResult<Chunk> {
-    let mut v: Vec<u8> = Vec::new();
-    let mut bytes: [u8; 1024] = [0; 1024];
-    let mut remain = usize::default();
-
-    while let Ok(read) = stream.read(&mut bytes) {
-        remain = read;
-
-        if read != 1024 {
-            break;
+        match message_type {
+            ChunkSize => self.decode_chunk_size(),
+            BytesRead => self.decode_bytes_read(),
+            Ping => self.decode_ping(),
+            ServerBandwidth => self.decode_server_bandwidth(),
+            ClientBandwidth => self.decode_client_bandwidth(),
+            Invoke => self.decode_invoke(),
+            _ => self.decode_unknown()
         }
-
-        v.extend_from_slice(&bytes);
     }
-
-    v.extend_from_slice(&bytes[..remain]);
-
-    let mut buffer = ByteBuffer::new(v);
-
-    buffer.decode_basic_header().and_then(
-        |basic_header| buffer.decode_message_header(&basic_header).map(
-            |message_header| {
-                let extended_timestamp = buffer.decode_extended_timestamp(&message_header);
-                let chunk_data = buffer.decode_chunk_data(&message_header);
-
-                Chunk::new(basic_header, message_header, extended_timestamp, chunk_data)
-                }
-            )
-    ).ok_or(
-        IOError::new(ErrorKind::InvalidInput, ChunkLengthError::new("The chunk is incomplete.".to_string(), None))
-    )
 }
