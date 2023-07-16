72bd3f6cd46bbca9b87b32cf892cd1073e284d73
t-matsudate <t.matsudate@gmail.com>
2023-07-16 18:10:03 +0900

    Added: A reader/writer to communicate chunk data with the partner.

--- sheave-core/src/readers/mod.rs
+++ sheave-core/src/readers/mod.rs
@@ -3,11 +3,13 @@ mod handshake;
 mod basic_header;
 mod message_header;
 mod extended_timestamp;
+mod chunk_data;
 
 pub use self::{
     encryption_algorithm::*,
     handshake::*,
     basic_header::*,
     message_header::*,
-    extended_timestamp::*
+    extended_timestamp::*,
+    chunk_data::*
 };
