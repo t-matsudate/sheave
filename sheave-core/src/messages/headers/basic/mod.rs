46874528bc9d1100094731a81afbf02cff80e62a
t-matsudate <t.matsudate@gmail.com>
2023-10-01 14:09:19 +0900

    Added: Document comments.

--- sheave-core/src/messages/headers/basic/mod.rs
+++ sheave-core/src/messages/headers/basic/mod.rs
@@ -17,7 +17,7 @@ pub use self::message_format::MessageFormat;
 /// Note if chunk ID is 16 bits, encoding/decoding it as Little Endian is required.
 ///
 /// Any Chunk ID which is and above 64 is required to add/subtract 64 from it when reading/writing.
-/// This considers a chunk ID which is replaced with the flag in first 1 byte.
+/// This means to compensate a 6 bits which were replaced with the flag.
 #[derive(Debug, Clone, Copy)]
 pub struct BasicHeader {
     message_format: MessageFormat,
@@ -25,14 +25,54 @@ pub struct BasicHeader {
 }
 
 impl BasicHeader {
+    /// Constructs a new basic header.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use sheave_core::messages::headers::{
+    ///     BasicHeader,
+    ///     MessageFormat
+    /// };
+    ///
+    /// BasicHeader::new(MessageFormat::New, u16::default());
+    /// ```
     pub fn new(message_format: MessageFormat, chunk_id: u16) -> Self {
         Self { message_format, chunk_id }
     }
 
+    /// Gets the message format.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use sheave_core::messages::headers::{
+    ///     BasicHeader,
+    ///     MessageFormat
+    /// };
+    ///
+    /// let message_format = MessageFormat::New;
+    /// let basic_header = BasicHeader::new(message_format, u16::default());
+    /// assert_eq!(message_format, basic_header.get_message_format())
+    /// ```
     pub fn get_message_format(&self) -> MessageFormat {
         self.message_format
     }
 
+    /// Gets the chunk ID.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use sheave_core::messages::headers::{
+    ///     BasicHeader,
+    ///     MessageFormat
+    /// };
+    ///
+    /// let chunk_id = u16::default();
+    /// let basic_header = BasicHeader::new(MessageFormat::New, chunk_id);
+    /// assert_eq!(chunk_id, basic_header.get_chunk_id())
+    /// ```
     pub fn get_chunk_id(&self) -> u16 {
         self.chunk_id
     }
