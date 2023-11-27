0e1fbf265b2e66da39114451dc9f208c4151f20a
t-matsudate <t.matsudate@gmail.com>
2023-11-27 19:18:13 +0900

    Added: Common CLI options: Protocols.

---
+++ sheave-core/src/cli/protocols/invalid_protocol.rs
@@ -0,0 +1,38 @@
+use std::{
+    error::Error,
+    fmt::{
+        Display,
+        Formatter,
+        Result as FormatResult
+    },
+    io::{
+        Error as IOError,
+        ErrorKind
+    }
+};
+
+/// Tells that specified protocol via CLI is invalid.
+#[derive(Debug)]
+pub struct InvalidProtocol(String);
+
+impl InvalidProtocol {
+    pub fn new(protocol: String) -> Self {
+        Self(protocol)
+    }
+}
+
+impl Display for InvalidProtocol {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FormatResult {
+        writeln!(f, "Protocol: This isn't one of RTMP: {}", self.0)
+    }
+}
+
+impl Error for InvalidProtocol {}
+
+/// A utility funciton for wrapping the error `InvalidProtocol` into `::std::io::Error`.
+pub fn invalid_protocol(protocol: &str) -> IOError {
+    IOError::new(
+        ErrorKind::InvalidInput,
+        InvalidProtocol::new(protocol.to_string())
+    )
+}
