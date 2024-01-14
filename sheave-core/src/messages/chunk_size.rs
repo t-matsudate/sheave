191cfe46c5ad25d80acd9465d42da4068fca9400
t-matsudate <t.matsudate@gmail.com>
2024-01-14 19:17:13 +0900

    Added: Documents and tests.

--- sheave-core/src/messages/chunk_size.rs
+++ sheave-core/src/messages/chunk_size.rs
@@ -13,6 +13,7 @@ use crate::{
 };
 pub use self::negative_chunk_size::*;
 
+/// Tells a size to chunk its stream to the partner.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct ChunkSize(u32);
 
@@ -20,15 +21,45 @@ impl ChunkSize {
     const NEGATIVE_FLAG: u32 = 0x80000000;
     const DEFAULT: u32 = 128;
 
+    /// Constructs a chunk size.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use sheave_core::messages::ChunkSize;
+    ///
+    /// let chunk_size = ChunkSize::new(128);
+    /// ```
     pub fn new(chunk_size: u32) -> Self {
         Self(chunk_size)
     }
 
+    /// Gets an internal value.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use sheave_core::messages::ChunkSize;
+    ///
+    /// let chunk_size = ChunkSize::new(128);
+    /// assert_eq!(128, chunk_size.get_chunk_size())
+    /// ```
     pub fn get_chunk_size(&self) -> u32 {
         self.0
     }
 }
 
+/// Constructs a ChunkSize with its default value.
+/// In RTMP, the default value of chunking size is defined to be 128.
+///
+/// # Examples
+///
+/// ```rust
+/// use sheave_core::messages::ChunkSize;
+///
+/// let chunk_size = ChunkSize::default();
+/// assert_eq!(128, chunk_size)
+/// ```
 impl Default for ChunkSize {
     fn default() -> Self {
         Self(Self::DEFAULT)
@@ -53,6 +84,43 @@ impl ChunkData for ChunkSize {
 }
 
 impl Decoder<ChunkSize> for ByteBuffer {
+    /// Decodes bytes into a ChunkSize.
+    ///
+    /// # Errors
+    ///
+    /// * [`Insufficientbufferlength`]
+    ///
+    /// When chunk data didn't remain at least 4 bytes.
+    ///
+    /// * [`NegativeChunkSize`]
+    ///
+    /// When its received chunk size's most significant bit is 1.
+    /// Chunking size is required that its bit is 0 in the specification.
+    /// This is probably considered of programs which has no unsigned type.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use sheave_core::{
+    ///     ByteBuffer,
+    ///     Decoder,
+    ///     messages::ChunkSize
+    /// };
+    ///
+    /// let mut buffer = ByteBuffer::default();
+    /// buffer.put_u32_be(128);
+    /// assert!(Decoder::<ChunkSize>::decode(&mut buffer).is_ok());
+    ///
+    /// let mut buffer = ByteBuffer::default();
+    /// buffer.put_u32_be(0x80000000);
+    /// assert!(Decoder::<ChunkSize>::decode(&mut buffer).is_err());
+    ///
+    /// let mut buffer = ByteBuffer::default();
+    /// assert!(Decoder::<ChunkSize>::decode(&mut buffer).is_err());
+    /// ```
+    ///
+    /// [`Insufficientbufferlength`]: crate::byte_buffer::InsufficientBufferLength
+    /// [`NegativeChunkSize`]: NegativeChunkSize
     fn decode(&mut self) -> IOResult<ChunkSize> {
         let chunk_size = self.get_u32_be()?;
 
@@ -65,7 +133,47 @@ impl Decoder<ChunkSize> for ByteBuffer {
 }
 
 impl Encoder<ChunkSize> for ByteBuffer {
+    /// Encodes a ChunkSize into bytes.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use sheave_core::{
+    ///     ByteBuffer,
+    ///     Encoder,
+    ///     messages::ChunkSize
+    /// };
+    ///
+    /// let mut buffer = ByteBuffer::default();
+    /// buffer.encode(&ChunkSize::default());
+    /// let chunk_size = buffer.get_u32_be().unwrap();
+    /// assert_eq!(128, chunk_size)
+    /// ```
     fn encode(&mut self, chunk_size: &ChunkSize) {
         self.put_u32_be(chunk_size.0);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn decode_chunk_size() {
+        let mut buffer = ByteBuffer::default();
+        buffer.put_u32_be(128);
+        assert!(Decoder::<ChunkSize>::decode(&mut buffer).is_ok());
+
+        let mut buffer = ByteBuffer::default();
+        buffer.put_u32_be(0x80000000);
+        assert!(Decoder::<ChunkSize>::decode(&mut buffer).is_err())
+    }
+
+    #[test]
+    fn encode_chunk_size() {
+        let mut buffer = ByteBuffer::default();
+        buffer.encode(&ChunkSize::default());
+        let chunk_size = buffer.get_u32_be().unwrap();
+        assert_eq!(128, chunk_size)
     }
 }
