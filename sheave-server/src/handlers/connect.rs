1c284adb3c15165ed1409c509f89b2c1dbdf2962
t-matsudate <t.matsudate@gmail.com>
2024-01-22 18:53:52 +0900

    Added: Document.

--- sheave-server/src/handlers/connect.rs
+++ sheave-server/src/handlers/connect.rs
@@ -35,9 +35,11 @@ use sheave_core::{
     writers::write_chunk,
 };
 
+#[doc(hidden)]
 #[derive(Debug)]
 pub struct ConnectHandler<'a, RW: AsyncRead + AsyncWrite + Unpin>(Pin<&'a mut RW>);
 
+#[doc(hidden)]
 impl<RW: AsyncRead + AsyncWrite + Unpin> AsyncHandler for ConnectHandler<'_, RW> {
     fn poll_handle(mut self: Pin<&mut Self>, cx: &mut FutureContext<'_>, rtmp_context: &mut RtmpContext) -> Poll<IOResult<()>> {
         let connect_request: Connect = ready!(pin!(read_chunk(self.0.as_mut(), rtmp_context)).poll(cx))?;
@@ -61,6 +63,77 @@ impl<RW: AsyncRead + AsyncWrite + Unpin> AsyncHandler for ConnectHandler<'_, RW>
     }
 }
 
+/// Handles a connect command as a server.
+///
+/// # Examples
+///
+/// ```rust
+/// use std::io::Result as IOResult;
+/// use futures::future::poll_fn;
+///
+/// #[tokio::main]
+/// async fn main() -> IOResult<()> {
+///     let result: IOResult<()> = poll_fn(
+///         |cx| {
+///             use std::{
+///                 future::Future,
+///                 pin::pin,
+///                 task::Poll,
+///                 time::Duration
+///             };
+///             use futures::ready;
+///             use sheave_core::{
+///                 handlers::{
+///                     AsyncHandler,
+///                     RtmpContext,
+///                     VecStream
+///                 },
+///                 messages::{
+///                     Connect,
+///                     ConnectResult,
+///                     amf::v0::{
+///                         Number,
+///                         AmfString
+///                     }
+///                 },
+///                 object,
+///                 readers::read_chunk,
+///                 writers::write_chunk
+///             };
+///             use sheave_server::handlers::handle_connect;
+///
+///             let mut stream = pin!(VecStream::default());
+///             let mut client_rtmp_context = RtmpContext::default();
+///             let mut server_rtmp_context = RtmpContext::default();
+///
+///             ready!(pin!(write_chunk(stream.as_mut(), &mut client_rtmp_context, Duration::default(), u32::default(), &Connect::default())).poll(cx))?;
+///
+///             ready!(pin!(handle_connect(stream.as_mut())).poll_handle(cx, &mut server_rtmp_context))?;
+///
+///             let actual: ConnectResult = ready!(pin!(read_chunk(stream.as_mut(), &mut client_rtmp_context)).poll(cx))?;
+///             let expected = ConnectResult::new(
+///                 "_result".into(),
+///                 object!(
+///                     "fmsVer" => AmfString::from("FMS/5,0,17"),
+///                     "capabilities" => Number::from(31)
+///                 ),
+///                 object!(
+///                     "level" => AmfString::from("status"),
+///                     "code" => AmfString::from("NetConnection.Connect.Success"),
+///                     "description" => AmfString::from("Connection succeeded."),
+///                     "objectEncoding" => Number::from(0)
+///                 )
+///             );
+///             assert_eq!(expected, actual);
+///
+///             Poll::Ready(Ok(()))
+///         }
+///     ).await;
+///     assert!(result.is_ok());
+///
+///     Ok(())
+/// }
+/// ```
 pub fn handle_connect<'a, RW: AsyncRead + AsyncWrite + Unpin>(stream: Pin<&'a mut RW>) -> ConnectHandler<'a, RW> {
     ConnectHandler(stream)
 }
