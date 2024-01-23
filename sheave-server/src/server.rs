66df0e293a91d9877de0ec95f6316ad1420317b6
t-matsudate <t.matsudate@gmail.com>
2024-01-23 18:22:29 +0900

    2 changes. See below lines.
    * Added: The steps to handle information excganges.
    * Added: Temporary: Echoes next unknown chunks for debugging.

--- sheave-server/src/server.rs
+++ sheave-server/src/server.rs
@@ -27,7 +27,10 @@ use sheave_core::{
 };
 use crate::handlers::{
     handle_first_handshake,
-    handle_second_handshake
+    handle_second_handshake,
+    handle_connect,
+    handle_release_stream,
+    handle_fc_publish
 };
 
 #[derive(Debug)]
@@ -52,6 +55,41 @@ impl<RW: AsyncRead + AsyncWrite + Unpin> Future for Server<RW> {
         pin!(
             handle_first_handshake(self.stream.make_weak_pin())
                 .chain(handle_second_handshake(self.stream.make_weak_pin()))
+                .chain(handle_connect(self.stream.make_weak_pin()))
+                .chain(handle_release_stream(self.stream.make_weak_pin()))
+                .chain(handle_fc_publish(self.stream.make_weak_pin()))
+                .chain(echo_next(self.stream.make_weak_pin()))
         ).poll_handle(cx, self.rtmp_context.make_weak_mut())
     }
+}
+
+#[derive(Debug)]
+struct EchoHandler<'a, RW: AsyncRead + AsyncWrite + Unpin>(Pin<&'a mut RW>);
+
+impl<RW: AsyncRead + AsyncWrite + Unpin> AsyncHandler for EchoHandler<'_, RW> {
+    fn poll_handle(mut self: Pin<&mut Self>, cx: &mut FutureContext<'_>, rtmp_context: &mut RtmpContext) -> Poll<IOResult<()>> {
+        use futures::ready;
+        use sheave_core::readers::{
+            read_basic_header,
+            read_message_header,
+            read_chunk_data
+        };
+        let basic_header = ready!(pin!(read_basic_header(self.0.as_mut())).poll(cx))?;
+        println!("{basic_header:?}");
+        let message_header = ready!(pin!(read_message_header(self.0.as_mut(), basic_header.get_message_format())).poll(cx))?;
+        println!("{message_header:?}");
+        let chunk_size = rtmp_context.get_receiving_chunk_size();
+        let message_length = if let Some(message_length) = message_header.get_message_length() {
+            message_length
+        } else {
+            rtmp_context.get_last_received_chunk(&basic_header.get_chunk_id()).unwrap().get_message_length()
+        };
+        let data = ready!(pin!(read_chunk_data(self.0.as_mut(), chunk_size, message_length)).poll(cx))?;
+        println!("{data:?}");
+        Poll::Ready(Ok(()))
+    }
+}
+
+fn echo_next<'a, RW: AsyncRead + AsyncWrite + Unpin>(stream: Pin<&'a mut RW>) -> EchoHandler<'a, RW> {
+    EchoHandler(stream)
 }
