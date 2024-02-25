50c15e11101e1aad04064e625e775bb847010a01
t-matsudate <t.matsudate@gmail.com>
2024-02-25 21:54:42 +0900

    Added: Explained about their traits simply.

--- sheave-core/src/decoder.rs
+++ sheave-core/src/decoder.rs
@@ -1,5 +1,7 @@
 use std::io::Result as IOResult;
 
+/// Decodes bytes into a chunk data.
+/// This can return errors for decoding fails by something cause.
 pub trait Decoder<T> {
     fn decode(&mut self) -> IOResult<T>;
 }
