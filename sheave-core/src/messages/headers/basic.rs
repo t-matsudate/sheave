4eef655fc88491eb144a49f849bf67f92879c468
t-matsudate <t.matsudate@gmail.com>
2024-04-04 18:40:42 +0900

    Removed: A redundant document.

--- sheave-core/src/messages/headers/basic.rs
+++ sheave-core/src/messages/headers/basic.rs
@@ -26,53 +26,16 @@ pub struct BasicHeader {
 
 impl BasicHeader {
     /// Constructs a new basic header.
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    /// use sheave_core::messages::headers::{
-    ///     BasicHeader,
-    ///     MessageFormat
-    /// };
-    ///
-    /// BasicHeader::new(MessageFormat::New, u16::default());
-    /// ```
     pub fn new(message_format: MessageFormat, chunk_id: u16) -> Self {
         Self { message_format, chunk_id }
     }
 
     /// Gets the message format.
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    /// use sheave_core::messages::headers::{
-    ///     BasicHeader,
-    ///     MessageFormat
-    /// };
-    ///
-    /// let message_format = MessageFormat::New;
-    /// let basic_header = BasicHeader::new(message_format, u16::default());
-    /// assert_eq!(message_format, basic_header.get_message_format())
-    /// ```
     pub fn get_message_format(&self) -> MessageFormat {
         self.message_format
     }
 
     /// Gets the chunk ID.
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    /// use sheave_core::messages::headers::{
-    ///     BasicHeader,
-    ///     MessageFormat
-    /// };
-    ///
-    /// let chunk_id = u16::default();
-    /// let basic_header = BasicHeader::new(MessageFormat::New, chunk_id);
-    /// assert_eq!(chunk_id, basic_header.get_chunk_id())
-    /// ```
     pub fn get_chunk_id(&self) -> u16 {
         self.chunk_id
     }
