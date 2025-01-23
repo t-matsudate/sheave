ea8cf298e3b8fe0f1b9b27aac47ef9afb0e62006
Tsuyoshi Matsudate <t.matsudate@gmail.com>
2025-01-23 09:26:54 +0000

    Added: Information level loggers, and ignoring in documentation because these are private modules.

--- sheave-server/src/handlers/middlewares/write_acknowledgement.rs
+++ sheave-server/src/handlers/middlewares/write_acknowledgement.rs
@@ -11,6 +11,7 @@ use std::{
     },
     time::Duration
 };
+use log::info;
 use futures::ready;
 use tokio::io::AsyncWrite;
 use sheave_core::{
@@ -29,9 +30,11 @@ use sheave_core::{
     writers::write_chunk
 };
 
+#[doc(hidden)]
 #[derive(Debug)]
 pub struct AcknowledgementWriter<'a, W: AsyncWrite + MeasureAcknowledgement + Unpin>(Pin<&'a mut W>);
 
+#[doc(hidden)]
 impl<W: AsyncWrite + MeasureAcknowledgement + Unpin> Middleware for AcknowledgementWriter<'_, W> {
     fn poll_handle_wrapped<H: AsyncHandler + Unpin>(mut self: Pin<&mut Self>, cx: &mut FutureContext<'_>, rtmp_context: &mut RtmpContext, handler: Pin<&mut H>) -> Poll<IOResult<()>> {
         self.0.begin_measuring();
@@ -43,13 +46,15 @@ impl<W: AsyncWrite + MeasureAcknowledgement + Unpin> Middleware for Acknowledgem
         if acknowledgement > (rtmp_context.get_window_acknowledgement_size() / 8) {
             let mut buffer = ByteBuffer::default();
             buffer.encode(&acknowledgement);
-            pin!(write_chunk(self.0.as_mut(), rtmp_context, Acknowledgement::CHANNEL.into(), Duration::default(), Acknowledgement::MESSAGE_TYPE, u32::default(), &Vec::<u8>::from(buffer))).poll(cx)
-        } else {
-            Poll::Ready(Ok(()))
+            ready!(pin!(write_chunk(self.0.as_mut(), rtmp_context, Acknowledgement::CHANNEL.into(), Duration::default(), Acknowledgement::MESSAGE_TYPE, u32::default(), &Vec::<u8>::from(buffer))).poll(cx))?;
+            info!("Acknowledgement got sent.");
         }
+
+        Poll::Ready(Ok(()))
     }
 }
 
+#[doc(hidden)]
 pub fn write_acknowledgement<'a, W: AsyncWrite + MeasureAcknowledgement + Unpin>(writer: Pin<&'a mut W>) -> AcknowledgementWriter<'a, W> {
     AcknowledgementWriter(writer)
 }
