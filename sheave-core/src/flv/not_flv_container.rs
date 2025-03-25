f928783e9465cf55d0eb7d67b2ee7cd5f3216780
Tsuyoshi Matsudate <t.matsudate@gmail.com>
2025-03-25 10:16:54 +0000

    Added: A doc comment.

--- sheave-core/src/flv/not_flv_container.rs
+++ sheave-core/src/flv/not_flv_container.rs
@@ -11,6 +11,7 @@ use std::{
     }
 };
 
+/// An error that specified file isn't the FLV format.
 #[derive(Debug)]
 pub struct NotFlvContainer(String);
 
@@ -22,6 +23,7 @@ impl Display for NotFlvContainer {
 
 impl Error for NotFlvContainer {}
 
+/// A utility function of constructing a `NotFlvContainer` error.
 pub fn not_flv_container<'a>(signature_bytes: &'a [u8]) -> IOError {
     let signature = String::from_utf8(signature_bytes.into()).unwrap();
     IOError::new(
