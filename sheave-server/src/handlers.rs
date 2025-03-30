27db2f62bf6921d0d8c1f3ab93b3f3ea040fc923
Tsuyoshi Matsudate <t.matsudate@gmail.com>
2025-03-30 08:36:39 +0000

    1 changed, 1 added:
    * Added: Topic distributor for subscribers.
    * Changed: Made errors more detailed where could be failed.

--- sheave-server/src/handlers.rs
+++ sheave-server/src/handlers.rs
@@ -1,4 +1,20 @@
 pub mod middlewares;
 mod rtmp;
+mod message_id_provider;
+mod topic_provider;
+mod undistinguishable_client;
+mod empty_playpath;
+mod metadata_not_found;
+mod stream_is_unpublished;
+mod inconsistent_playpath;
 
 pub use self::rtmp::RtmpHandler;
+use self::{
+    message_id_provider::*,
+    topic_provider::*,
+    undistinguishable_client::*,
+    empty_playpath::*,
+    metadata_not_found::*,
+    stream_is_unpublished::*,
+    inconsistent_playpath::*
+};
