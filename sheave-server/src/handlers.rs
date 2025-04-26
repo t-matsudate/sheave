331d45f5e18fc134ce1e29d8dc5d260cd0b04102
Tsuyoshi Matsudate <t.matsudate@gmail.com>
2025-04-26 09:45:11 +0000

    Changed: FLV files treat as topics of the publisher-subscriber model.

--- sheave-server/src/handlers.rs
+++ sheave-server/src/handlers.rs
@@ -2,19 +2,21 @@ pub mod middlewares;
 mod rtmp;
 mod message_id_provider;
 mod topic_provider;
+mod inconsistent_app_path;
 mod undistinguishable_client;
-mod empty_playpath;
+mod empty_topic_path;
 mod metadata_not_found;
 mod stream_is_unpublished;
-mod inconsistent_playpath;
+mod inconsistent_topic_path;
 
 pub use self::rtmp::RtmpHandler;
 use self::{
     message_id_provider::*,
     topic_provider::*,
+    inconsistent_app_path::*,
     undistinguishable_client::*,
-    empty_playpath::*,
+    empty_topic_path::*,
     metadata_not_found::*,
     stream_is_unpublished::*,
-    inconsistent_playpath::*
+    inconsistent_topic_path::*
 };
