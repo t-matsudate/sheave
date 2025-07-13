66283805476657af3d0eb10b93fbc7e6bfa40c76
Tsuyoshi Matsudate <t.matsudate@gmail.com>
2025-07-13 10:04:34 +0000

    Fixed: Changes of the constructor of `Play`.

--- sheave-server/src/handlers/rtmp.rs
+++ sheave-server/src/handlers/rtmp.rs
@@ -1392,7 +1392,7 @@ mod tests {
         rtmp_context.set_message_id(0);
 
         let mut buffer = ByteBuffer::default();
-        buffer.encode(&Play::new(AmfString::new(Uuid::now_v7().to_string()), Number::from(-2u8));
+        buffer.encode(&Play::new(AmfString::new(Uuid::now_v7().to_string()), Number::from(-2u8)));
         handle_message(stream.as_mut()).handle_play_request(&mut rtmp_context, buffer).await.unwrap();
         let result = handle_message(stream.as_mut()).write_play_response(&mut rtmp_context).await;
         assert!(result.is_err());
